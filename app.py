2ee274a8f06657ecb6e19e166620f54a2886b697
Njilo <nysocikwa2004@gmail.com>
2025-06-10 13:36:59 +0100

    FInal commit of the day at the office

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask, render_template
+import time
+import board
 
 try: 
     import adafruit_dht
     DHT = True
     print("Successful import of DHT")
+    dht = adafruit_dht.DHT22(board.D4)
 except:
     DHT = False
     print("Unsuccessful import of DHT")
@@ -13,14 +16,20 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
+
     if DHT:
-        humidity = adafruit_dht.DHT22.temperature
-        temperature = adafruit_dht.DHT22.temperature
+        try:
+            humidity = dht.humidity
+            temperature = dht.temperature
+            time.sleep(2)
+            print("The temp is {} degrees C and the humidity is {}\n".format(temperature, humidity))
+        except:
+                print("error")
     else:
-        humidity = -1
-        temperature = -1
+        print("error")
     
+        
     return render_template("index.html", temp=temperature, humidity = humidity)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=8000)
